83e504706810dde61631ac29554ff7e9e9b632a5
Jihyun/Blake La <late@usc.edu>
2025-02-14 15:15:16 -0800

    Metronome without flashing

---
+++ Assets/Metronome_Flash.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome_Flash : MonoBehaviour
+{
+    float timeElapsed = 0;
+    float lerpDuration = 4;
+
+    Vector3 startPosition = new Vector3(-8.00f, 3.00f, 0.00f);
+    Vector3 endPosition = new Vector3(0.00f, 3.00f, 0.00f);
+
+    //float lerpedValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 startPosition = transform.position;
+        //Debug.Log(startPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeElapsed <= lerpDuration)
+        {
+            transform.position = Vector3.Lerp(startPosition, endPosition, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            Debug.Log(timeElapsed);
+            if (timeElapsed >= lerpDuration)
+            {
+                Debug.Log("Test");
+                timeElapsed = 0.00f;
+                transform.position = startPosition;
+                Debug.Log(transform.position);
+            }
+        }
+    }
+}
