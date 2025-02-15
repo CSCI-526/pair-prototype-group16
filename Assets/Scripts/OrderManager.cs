62c99158157d813aba2ec1adbda2877101991d5a
jingxuawu3 <jingxuawu3-c@my.cityu.edu.hk>
2025-02-14 16:19:32 -0800

    Test add boxes

---
+++ Assets/Scripts/OrderManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public class Order
+{
+    public int circles; // num of circles
+    public int squares; // num of squares
+
+    public Order(int circles, int squares)
+    {
+        this.circles = circles;
+        this.squares = squares;
+    }
+
+    public override string ToString()
+    {
+        return $"{circles} Circles, {squares} Squares";
+    }
+}
+
+public class OrderManager : MonoBehaviour
+{
+    public Order[] orders; // all availble orders
+    [SerializeField] private TMP_Text orderDisplay; // displayed order
+    public Button[] orderButtons; // choose button
+
+    private Order currentOrder; // current order
+    private int currentCircles = 0; // num of circles
+    private int currentSquares = 0; // num of squares
+
+    private void Start()
+    {
+        // initialize
+        orders = new Order[]
+        {
+            new Order(3, 2), // order 1: 3 circles, 2 squares
+            new Order(3, 1)  // order 2: 3 circles, 1 squares
+        };
+
+        // add click 
+        for (int i = 0; i < orderButtons.Length; i++)
+        {
+            int index = i; // get index
+            orderButtons[i].onClick.AddListener(() => SelectOrder(index));
+        }
+    }
+
+    public void SelectOrder(int index)
+    {
+        if (index >= 0 && index < orders.Length)
+        {
+            currentOrder = orders[index];
+            currentCircles = currentOrder.circles;
+            currentSquares = currentOrder.squares;
+            UpdateOrderDisplay();
+        }
+    }
+
+    private void UpdateOrderDisplay()
+    {
+        if (orderDisplay != null)
+        {
+            orderDisplay.text = currentOrder.ToString();
+        }
+        else
+        {
+            Debug.LogError("orderDisplay not be settled");
+        }
+    }
+}
